596574686c5e848d638eb04dc09c4aa2795927d6
naim0018 <mdkazinaim0018@gmail.com>
2025-11-16 21:56:20 +0600

    update

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    build: {
-    outDir: ".",        // Put build output into root
-    emptyOutDir: false, // Prevent Vite from deleting source files
-    assetsDir: "assets" // Keep JS/CSS/images organized
-  }
 })
